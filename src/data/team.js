b07026595bf323d1031affaf11284500ea761370
Fernando Giganto <fernando.giganto@sotysolar.es>
2026-06-08 09:00:00 +0200

    pruebas nuevas

---
+++ src/data/team.js
@@ -0,0 +1,14 @@
+export const TEAM_NUMBER = '09';
+
+export const TEAM_MEMBERS = [
+  {
+    name: 'Fernando Giganto',
+    contribution:
+      'Desarrollo de funcionalidades, integración de la aplicación y automatización de pruebas.',
+  },
+  {
+    name: 'James Iglesias Fernandezpolo',
+    contribution:
+      'Organización, creación y maquetación de la documentación de la actividad.',
+  },
+];
